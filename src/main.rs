bf8da1b7479d742e984b257891f225beae625d37
Sylvain Garant <sylvain.garant@protonmail.com>
2025-05-10 17:14:48 +0200

    Ça fonctionne mais faut pas trop toucher mdrrr

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use std::{io, path::PathBuf};
+use std::{env, fs, io, os::unix::ffi::OsStrExt, path::PathBuf};
 
 use clap::Parser;
 use image::ImageReader;
 
-const NULL_PIXEL: image::Rgba<u8> = image::Rgba([0, 0, 0, 0]);
+const USIZE_LEN: usize = usize::BITS as usize / 8;
+// const NULL_PIXEL: image::Rgba<u8> = image::Rgba([0, 0, 0, 0]);
+
+// https://linuxcommandlibrary.com/man/steghide
+// https://jsmnbom.github.io/toki-pona-helper/sitelenPonaConverter.html
 
 #[derive(Debug, thiserror::Error)]
 enum Error {
@@ -11,6 +15,8 @@ enum Error {
     Io(#[from] io::Error),
     #[error("Image error: {0}")]
     Image(#[from] image::error::ImageError),
+    // #[error("Bincode encoding error: {0}")]
+    // BincodeEncoding(#[from] bincode::error::EncodeError),
 }
 
 /// Smaragdina
@@ -22,59 +28,163 @@ struct Args {
 
 #[derive(Parser)]
 enum Command {
-    Hide {
-        message: String,
+    Embed {
+        /// Path to the cover file in which to embed data
         #[arg(short, long)]
-        image: PathBuf,
+        cover_file: PathBuf,
+        /// Path to the data file to be hidden
         #[arg(short, long)]
-        output: PathBuf,
+        embed_file: PathBuf,
+        /// Path to the stego file (containing hidden data) for extraction
+        #[arg(short, long)]
+        stego_file: PathBuf,
     },
-    Print {
+    Extract {
+        /// Path to the stego file (containing hidden data) for extraction
         #[arg(short, long)]
-        image: PathBuf,
+        stego_file: PathBuf,
+        /// Path to directory where the embed tile will be extracted
+        #[arg(short, long)]
+        destination: Option<PathBuf>,
     },
 }
 
 impl Command {
-    #[allow(clippy::many_single_char_names)]
-    fn execute(&self) -> Result<(), Error> {
+    fn execute(self) -> Result<(), Error> {
         match self {
-            Command::Hide {
-                message,
-                image,
-                output,
+            Command::Embed {
+                cover_file,
+                embed_file,
+                stego_file,
             } => {
-                let mut img = ImageReader::open(image)?.decode()?.to_rgba8();
-                let mut message = message.bytes();
+                let mut img = ImageReader::open(cover_file)?.decode()?.to_rgba8();
+                // let mut sekreto = <&Sekreto as TryInto<Vec<u8>>>::try_into(&Sekreto::try_from(
+                //     embed_file.as_path(),
+                // )?)?;
+                // sekreto.push(0);
+                let embed = fs::read(&embed_file)?;
+                let embed = [
+                    embed.len().to_be_bytes().to_vec(),
+                    [embed_file.file_name().unwrap().as_bytes(), &[0]].concat(),
+                    embed,
+                ]
+                .concat();
 
-                for pixel in img.pixels_mut() {
-                    let Some(b) = message.next() else {
-                        *pixel = NULL_PIXEL;
-                        break;
-                    };
+                for (pixel, b) in img.pixels_mut().zip(embed.iter()) {
                     let image::Rgba(rgba) = pixel;
-                    *pixel = image::Rgba(encode(*rgba, b));
+                    *pixel = image::Rgba(encode(*rgba, *b));
                 }
 
-                img.save(output)?;
+                img.save(stego_file)?;
             }
-            Command::Print { image } => {
-                let img = ImageReader::open(image)?.decode()?.to_rgba8();
-                let mut message = String::new();
+            Command::Extract {
+                stego_file,
+                destination,
+            } => {
+                let img = ImageReader::open(stego_file)?.decode()?.to_rgba8();
+                let mut embed = Vec::new();
 
-                for pixel in img.pixels() {
-                    if *pixel == NULL_PIXEL {
+                let mut pixels = img.pixels();
+                let embed_len = usize::from_be_bytes(
+                    <Vec<u8> as TryInto<[u8; USIZE_LEN]>>::try_into(
+                        (0..USIZE_LEN)
+                            .filter_map(|_| pixels.next())
+                            .map(|image::Rgba(rgba)| decode(*rgba))
+                            .collect::<Vec<u8>>(),
+                    )
+                    .unwrap(),
+                );
+
+                let mut embed_file = String::new();
+                loop {
+                    let image::Rgba(rgba) = pixels
+                        .next()
+                        .expect("Failed to read the name of the embed file");
+                    let b = decode(*rgba);
+                    if b == 0 {
                         break;
                     }
-                    let image::Rgba(rgba) = pixel;
-                    message.push(decode(*rgba) as char);
+                    embed_file.push(decode(*rgba) as char);
                 }
-                println!("{message}");
+
+                for (pixel, _) in pixels.zip(0..embed_len) {
+                    let image::Rgba(rgba) = pixel;
+                    embed.push(decode(*rgba));
+                }
+
+                fs::write(
+                    if let Some(destination) = destination {
+                        destination
+                    } else {
+                        env::current_dir().unwrap()
+                    }
+                    .join(embed_file),
+                    embed,
+                )?;
             }
         }
         Ok(())
     }
 }
+
+// #[derive(bincode::Encode, bincode::Decode)]
+// struct Sekreto {
+//     r#type: FileType,
+//     name: String,
+//     content: Vec<u8>,
+// }
+
+// #[derive(bincode::Encode, bincode::Decode)]
+// enum FileType {
+//     GrayscaleImage((u32, u32)),
+//     Raw,
+// }
+
+// impl TryFrom<&Path> for Sekreto {
+//     type Error = Error;
+
+//     fn try_from(value: &Path) -> Result<Self, Self::Error> {
+//         Ok(match ImageReader::open(value)?.decode() {
+//             Ok(image) => Self {
+//                 r#type: FileType::GrayscaleImage((0, 0)),
+//                 name: value.file_name().unwrap().to_str().unwrap().to_string(),
+//                 content: image.grayscale().as_bytes().to_vec(),
+//             },
+//             Err(_) => Self {
+//                 r#type: FileType::Raw,
+//                 name: value.file_name().unwrap().to_str().unwrap().to_string(),
+//                 content: fs::read(value)?,
+//             },
+//         })
+//     }
+// }
+
+// impl TryFrom<&Sekreto> for Vec<u8> {
+//     type Error = bincode::error::EncodeError;
+
+//     fn try_from(value: &Sekreto) -> Result<Self, Self::Error> {
+//         bincode::encode_to_vec(
+//             value,
+//             bincode::config::standard()
+//                 .with_big_endian()
+//                 .with_fixed_int_encoding(),
+//         )
+//     }
+// }
+
+// impl TryFrom<Vec<u8>> for Sekreto {
+//     type Error = bincode::error::DecodeError;
+
+//     fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+//         let (this, _) = bincode::decode_from_slice(
+//             &value,
+//             bincode::config::standard()
+//                 .with_big_endian()
+//                 .with_fixed_int_encoding(),
+//         )?;
+//         Ok(this)
+//     }
+// }
 
 fn encode(rgba: [u8; 4], byte: u8) -> [u8; 4] {
     let [r, g, b, a] = rgba;
