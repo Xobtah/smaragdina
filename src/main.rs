24f2622436b1b18ae1b10b1e26aee6747381d33b
Sylvain Garant <sylvain.garant@protonmail.com>
2025-05-10 21:09:49 +0200

    C'est un peu mieux

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{env, fs, io, os::unix::ffi::OsStrExt, path::PathBuf};
+use std::{
+    env, fs, io,
+    path::{Path, PathBuf},
+};
 
 use clap::Parser;
 use image::ImageReader;
@@ -15,37 +18,39 @@ enum Error {
     Io(#[from] io::Error),
     #[error("Image error: {0}")]
     Image(#[from] image::error::ImageError),
-    // #[error("Bincode encoding error: {0}")]
-    // BincodeEncoding(#[from] bincode::error::EncodeError),
+    #[error("Bincode encoding error: {0}")]
+    BincodeEncoding(#[from] bincode::error::EncodeError),
+    #[error("Bincode decoding error: {0}")]
+    BincodeDecoding(#[from] bincode::error::DecodeError),
 }
 
 /// Smaragdina
 #[derive(Parser)]
 struct Args {
     #[command(subcommand)]
-    command: Command,
+    ordo: Command,
 }
 
 #[derive(Parser)]
 enum Command {
     Embed {
         /// Path to the cover file in which to embed data
-        #[arg(short, long)]
-        cover_file: PathBuf,
+        #[arg(short = 'c', long = "cover_file")]
+        kovrilodosiero: PathBuf,
         /// Path to the data file to be hidden
-        #[arg(short, long)]
-        embed_file: PathBuf,
+        #[arg(short = 'e', long = "embed_file")]
+        dosierenhavo: PathBuf,
         /// Path to the stego file (containing hidden data) for extraction
-        #[arg(short, long)]
-        stego_file: PathBuf,
+        #[arg(short = 's', long = "stego_file")]
+        stegodosiero: PathBuf,
     },
     Extract {
         /// Path to the stego file (containing hidden data) for extraction
-        #[arg(short, long)]
-        stego_file: PathBuf,
+        #[arg(short = 's', long = "stego_file")]
+        stegodosiero: PathBuf,
         /// Path to directory where the embed tile will be extracted
-        #[arg(short, long)]
-        destination: Option<PathBuf>,
+        #[arg(short = 'd', long = "dest")]
+        celloko: Option<PathBuf>,
     },
 }
 
@@ -53,73 +58,51 @@ impl Command {
     fn execute(self) -> Result<(), Error> {
         match self {
             Command::Embed {
-                cover_file,
-                embed_file,
-                stego_file,
+                kovrilodosiero,
+                dosierenhavo,
+                stegodosiero,
             } => {
-                let mut img = ImageReader::open(cover_file)?.decode()?.to_rgba8();
-                // let mut sekreto = <&Sekreto as TryInto<Vec<u8>>>::try_into(&Sekreto::try_from(
-                //     embed_file.as_path(),
-                // )?)?;
-                // sekreto.push(0);
-                let embed = fs::read(&embed_file)?;
-                let embed = [
-                    embed.len().to_be_bytes().to_vec(),
-                    [embed_file.file_name().unwrap().as_bytes(), &[0]].concat(),
-                    embed,
-                ]
-                .concat();
+                let mut bildo = ImageReader::open(kovrilodosiero)?.decode()?.to_rgba8();
+                let sekreto = Sekreto::try_from(dosierenhavo.as_path())?;
+                let ensxipigxis = <&Sekreto as TryInto<Vec<u8>>>::try_into(&sekreto)?;
+                let ensxipigxis = [ensxipigxis.len().to_le_bytes().to_vec(), ensxipigxis].concat();
 
-                for (pixel, b) in img.pixels_mut().zip(embed.iter()) {
-                    let image::Rgba(rgba) = pixel;
-                    *pixel = image::Rgba(encode(*rgba, *b));
+                for (pikselo, b) in bildo.pixels_mut().zip(ensxipigxis.iter()) {
+                    let image::Rgba(rgba) = pikselo;
+                    *pikselo = image::Rgba(cxifri_bajton(*rgba, *b));
                 }
 
-                img.save(stego_file)?;
+                bildo.save(stegodosiero)?;
             }
             Command::Extract {
-                stego_file,
-                destination,
+                stegodosiero,
+                celloko,
             } => {
-                let img = ImageReader::open(stego_file)?.decode()?.to_rgba8();
-                let mut embed = Vec::new();
+                let bildo = ImageReader::open(stegodosiero)?.decode()?.to_rgba8();
+                let mut pikseloj = bildo.pixels();
 
-                let mut pixels = img.pixels();
-                let embed_len = usize::from_be_bytes(
+                let ensxipigxis_lon = usize::from_le_bytes(
                     <Vec<u8> as TryInto<[u8; USIZE_LEN]>>::try_into(
                         (0..USIZE_LEN)
-                            .filter_map(|_| pixels.next())
-                            .map(|image::Rgba(rgba)| decode(*rgba))
+                            .filter_map(|_| pikseloj.next())
+                            .map(|image::Rgba(rgba)| decxifri_bajton(*rgba))
                             .collect::<Vec<u8>>(),
                     )
                     .unwrap(),
                 );
 
-                let mut embed_file = String::new();
-                loop {
-                    let image::Rgba(rgba) = pixels
-                        .next()
-                        .expect("Failed to read the name of the embed file");
-                    let b = decode(*rgba);
-                    if b == 0 {
-                        break;
-                    }
-                    embed_file.push(decode(*rgba) as char);
-                }
-
-                for (pixel, _) in pixels.zip(0..embed_len) {
-                    let image::Rgba(rgba) = pixel;
-                    embed.push(decode(*rgba));
-                }
+                let Sekreto { dosiero, enhavo } = Sekreto::try_from(
+                    pikseloj
+                        .zip(0..ensxipigxis_lon)
+                        .map(|(image::Rgba(rgba), _)| decxifri_bajton(*rgba))
+                        .collect::<Vec<_>>(),
+                )?;
 
                 fs::write(
-                    if let Some(destination) = destination {
-                        destination
-                    } else {
-                        env::current_dir().unwrap()
-                    }
-                    .join(embed_file),
-                    embed,
+                    celloko
+                        .unwrap_or_else(|| env::current_dir().unwrap())
+                        .join(dosiero),
+                    enhavo,
                 )?;
             }
         }
@@ -127,118 +110,103 @@ impl Command {
     }
 }
 
-// #[derive(bincode::Encode, bincode::Decode)]
-// struct Sekreto {
-//     r#type: FileType,
-//     name: String,
-//     content: Vec<u8>,
-// }
+#[derive(bincode::Encode, bincode::Decode)]
+struct Sekreto {
+    dosiero: String,
+    enhavo: Vec<u8>,
+}
 
-// #[derive(bincode::Encode, bincode::Decode)]
-// enum FileType {
-//     GrayscaleImage((u32, u32)),
-//     Raw,
-// }
+impl Sekreto {
+    const fn bincode_config() -> bincode::config::Configuration {
+        bincode::config::standard()
+            .with_little_endian()
+            .with_variable_int_encoding()
+    }
+}
 
-// impl TryFrom<&Path> for Sekreto {
-//     type Error = Error;
+impl TryFrom<&Path> for Sekreto {
+    type Error = io::Error;
 
-//     fn try_from(value: &Path) -> Result<Self, Self::Error> {
-//         Ok(match ImageReader::open(value)?.decode() {
-//             Ok(image) => Self {
-//                 r#type: FileType::GrayscaleImage((0, 0)),
-//                 name: value.file_name().unwrap().to_str().unwrap().to_string(),
-//                 content: image.grayscale().as_bytes().to_vec(),
-//             },
-//             Err(_) => Self {
-//                 r#type: FileType::Raw,
-//                 name: value.file_name().unwrap().to_str().unwrap().to_string(),
-//                 content: fs::read(value)?,
-//             },
-//         })
-//     }
-// }
+    fn try_from(valoro: &Path) -> Result<Self, Self::Error> {
+        Ok(Self {
+            dosiero: valoro.file_name().unwrap().to_str().unwrap().to_string(),
+            enhavo: fs::read(valoro)?,
+        })
+    }
+}
 
-// impl TryFrom<&Sekreto> for Vec<u8> {
-//     type Error = bincode::error::EncodeError;
+impl TryFrom<&Sekreto> for Vec<u8> {
+    type Error = bincode::error::EncodeError;
 
-//     fn try_from(value: &Sekreto) -> Result<Self, Self::Error> {
-//         bincode::encode_to_vec(
-//             value,
-//             bincode::config::standard()
-//                 .with_big_endian()
-//                 .with_fixed_int_encoding(),
-//         )
-//     }
-// }
+    fn try_from(valoro: &Sekreto) -> Result<Self, Self::Error> {
+        bincode::encode_to_vec(valoro, Sekreto::bincode_config())
+    }
+}
 
-// impl TryFrom<Vec<u8>> for Sekreto {
-//     type Error = bincode::error::DecodeError;
+impl TryFrom<Vec<u8>> for Sekreto {
+    type Error = bincode::error::DecodeError;
 
-//     fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
-//         let (this, _) = bincode::decode_from_slice(
-//             &value,
-//             bincode::config::standard()
-//                 .with_big_endian()
-//                 .with_fixed_int_encoding(),
-//         )?;
-//         Ok(this)
-//     }
-// }
+    fn try_from(valoro: Vec<u8>) -> Result<Self, Self::Error> {
+        let (tio, _) = bincode::decode_from_slice(&valoro, Sekreto::bincode_config())?;
+        Ok(tio)
+    }
+}
 
-fn encode(rgba: [u8; 4], byte: u8) -> [u8; 4] {
+fn cxifri_bajton(rgba: [u8; 4], bajto: u8) -> [u8; 4] {
     let [r, g, b, a] = rgba;
     [
-        (r & 0xFC) + ((byte & 0xC0) >> 6),
-        (g & 0xFC) + ((byte & 0x30) >> 4),
-        (b & 0xFC) + ((byte & 0x0C) >> 2),
-        (a & 0xFC) + (byte & 0x03),
+        (r & 0xFC) + ((bajto & 0xC0) >> 6),
+        (g & 0xFC) + ((bajto & 0x30) >> 4),
+        (b & 0xFC) + ((bajto & 0x0C) >> 2),
+        (a & 0xFC) + (bajto & 0x03),
     ]
 }
 
-fn decode(rgba: [u8; 4]) -> u8 {
+fn decxifri_bajton(rgba: [u8; 4]) -> u8 {
     let [r, g, b, a] = rgba;
     ((r & 0x03) << 6) + ((g & 0x03) << 4) + ((b & 0x03) << 2) + (a & 0x03)
 }
 
 fn main() -> Result<(), Error> {
-    Args::parse().command.execute()
+    Args::parse().ordo.execute()
 }
 
 #[cfg(test)]
 mod tests {
+    use super::{cxifri_bajton as cxifri, decxifri_bajton as decxifri};
+
     #[test]
     fn encode_empty_pixel() {
-        assert_eq!(super::encode([0, 0, 0, 0], 0b0000_0000), [0, 0, 0, 0]);
-        assert_eq!(super::encode([0, 0, 0, 0], 0b0000_0001), [0, 0, 0, 1]);
-        assert_eq!(super::encode([0, 0, 0, 0], 0b0000_0010), [0, 0, 0, 2]);
-        assert_eq!(super::encode([0, 0, 0, 0], 0b0000_0100), [0, 0, 1, 0]);
-        assert_eq!(super::encode([0, 0, 0, 0], 0b0000_1000), [0, 0, 2, 0]);
-        assert_eq!(super::encode([0, 0, 0, 0], 0b0001_0000), [0, 1, 0, 0]);
-        assert_eq!(super::encode([0, 0, 0, 0], 0b0010_0000), [0, 2, 0, 0]);
-        assert_eq!(super::encode([0, 0, 0, 0], 0b0100_0000), [1, 0, 0, 0]);
-        assert_eq!(super::encode([0, 0, 0, 0], 0b1000_0000), [2, 0, 0, 0]);
-        assert_eq!(super::encode([0, 0, 0, 0], 0b1111_1111), [3, 3, 3, 3]);
+        assert_eq!(cxifri([0, 0, 0, 0], 0b0000_0000), [0, 0, 0, 0]);
+        assert_eq!(cxifri([0, 0, 0, 0], 0b0000_0001), [0, 0, 0, 1]);
+        assert_eq!(cxifri([0, 0, 0, 0], 0b0000_0010), [0, 0, 0, 2]);
+        assert_eq!(cxifri([0, 0, 0, 0], 0b0000_0100), [0, 0, 1, 0]);
+        assert_eq!(cxifri([0, 0, 0, 0], 0b0000_1000), [0, 0, 2, 0]);
+        assert_eq!(cxifri([0, 0, 0, 0], 0b0001_0000), [0, 1, 0, 0]);
+        assert_eq!(cxifri([0, 0, 0, 0], 0b0010_0000), [0, 2, 0, 0]);
+        assert_eq!(cxifri([0, 0, 0, 0], 0b0100_0000), [1, 0, 0, 0]);
+        assert_eq!(cxifri([0, 0, 0, 0], 0b1000_0000), [2, 0, 0, 0]);
+        assert_eq!(cxifri([0, 0, 0, 0], 0b1111_1111), [3, 3, 3, 3]);
     }
 
     #[test]
     fn decode() {
-        assert_eq!(super::decode([0, 0, 0, 0]), 0);
-        assert_eq!(super::decode([0, 0, 0, 1]), 1);
-        assert_eq!(super::decode([0, 0, 0, 2]), 2);
-        assert_eq!(super::decode([0, 0, 0, 3]), 3);
-        assert_eq!(super::decode([0, 0, 0, 4]), 0);
-        assert_eq!(super::decode([0, 0, 1, 0]), 4);
-        assert_eq!(super::decode([0, 0, 2, 0]), 8);
-        assert_eq!(super::decode([0, 0, 3, 0]), 12);
-        assert_eq!(super::decode([0, 0, 4, 0]), 0);
-        assert_eq!(super::decode([0, 1, 0, 0]), 16);
-        assert_eq!(super::decode([0, 2, 0, 0]), 32);
-        assert_eq!(super::decode([0, 3, 0, 0]), 48);
-        assert_eq!(super::decode([0, 4, 0, 0]), 0);
-        assert_eq!(super::decode([1, 0, 0, 0]), 64);
-        assert_eq!(super::decode([2, 0, 0, 0]), 128);
-        assert_eq!(super::decode([3, 0, 0, 0]), 192);
-        assert_eq!(super::decode([4, 0, 0, 0]), 0);
+        assert_eq!(decxifri([0, 0, 0, 0]), 0);
+        assert_eq!(decxifri([0, 0, 0, 1]), 1);
+        assert_eq!(decxifri([0, 0, 0, 2]), 2);
+        assert_eq!(decxifri([0, 0, 0, 3]), 3);
+        assert_eq!(decxifri([0, 0, 0, 4]), 0);
+        assert_eq!(decxifri([0, 0, 1, 0]), 4);
+        assert_eq!(decxifri([0, 0, 2, 0]), 8);
+        assert_eq!(decxifri([0, 0, 3, 0]), 12);
+        assert_eq!(decxifri([0, 0, 4, 0]), 0);
+        assert_eq!(decxifri([0, 1, 0, 0]), 16);
+        assert_eq!(decxifri([0, 2, 0, 0]), 32);
+        assert_eq!(decxifri([0, 3, 0, 0]), 48);
+        assert_eq!(decxifri([0, 4, 0, 0]), 0);
+        assert_eq!(decxifri([1, 0, 0, 0]), 64);
+        assert_eq!(decxifri([2, 0, 0, 0]), 128);
+        assert_eq!(decxifri([3, 0, 0, 0]), 192);
+        assert_eq!(decxifri([4, 0, 0, 0]), 0);
     }
 }
